d64a0eaf13cc053cf0106145b6c0b66d948f6258
anthrofox22 <anthrofox22@protonmail.com>
2024-08-19 17:11:39 +1000

    fix pages deploy again

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/service-worker.js')
+      .register('/joi.how/service-worker.js')
       .catch(registrationError => {
         console.error('SW registration failed: ', registrationError);
       });
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import { GamePage } from '../game';
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/play' element={<GamePage />} />
